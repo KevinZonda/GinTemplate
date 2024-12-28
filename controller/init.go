cd68a5ffacf428bc364e8eb9c992c63fe70a5917
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2024-12-28 09:55:52 +0800

    fix

--- controller/init.go
+++ controller/init.go
@@ -3,20 +3,15 @@ package controller
 import (
 	"github.com/KevinZonda/GinTemplate/controller/ping"
 	"github.com/KevinZonda/GinTemplate/controller/types"
-	"github.com/KevinZonda/GinTemplate/share"
 	"github.com/gin-gonic/gin"
 )
 
-func Init(engine gin.IRouter) {
-	engine.GET("/ping", func(c *gin.Context) {
-		c.String(204, "pong")
-	})
-
-	register(&ping.Controller{})
+func Init(r gin.IRouter) {
+	register(r, &ping.Controller{})
 }
 
-func register(cs ...types.IController) {
+func register(r gin.IRouter, cs ...types.IController) {
 	for _, c := range cs {
-		c.Init(share.Engine)
+		c.Init(r)
 	}
 }
